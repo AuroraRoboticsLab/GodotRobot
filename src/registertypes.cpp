6b8b38730f6d0f71325d049b89e034b90368e1ec
Orion Lawlor <lawlor@alaska.edu>
2024-03-13 19:09:57 -0800

    Initial checkin.  This is mostly following the GDExtension tutorial https://docs.godotengine.org/en/stable/tutorials/scripting/gdextension/gdextension_cpp_example.html

---
+++ src/registertypes.cpp
@@ -0,0 +1,44 @@
+/*
+Register GodotRobot classes with Godot.
+
+Taken directly from GDExtension tutorial:
+  https://docs.godotengine.org/en/stable/tutorials/scripting/gdextension/gdextension_cpp_example.html
+
+*/
+#include "godotrobot.h"
+
+#include <gdextension_interface.h>
+#include <godot_cpp/core/class_db.hpp>
+#include <godot_cpp/core/defs.hpp>
+#include <godot_cpp/godot.hpp>
+
+using namespace godot;
+
+void initialize_godotrobot_module(ModuleInitializationLevel p_level) {
+	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
+		return;
+	}
+
+	ClassDB::register_class<GodotRobot2D>();
+}
+
+void uninitialize_godotrobot_module(ModuleInitializationLevel p_level) {
+	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
+		return;
+	}
+}
+
+extern "C" {
+// Initialization.
+GDExtensionBool GDE_EXPORT godotrobot_library_init(GDExtensionInterfaceGetProcAddress p_get_proc_address, const GDExtensionClassLibraryPtr p_library, GDExtensionInitialization *r_initialization) {
+	godot::GDExtensionBinding::InitObject init_obj(p_get_proc_address, p_library, r_initialization);
+
+	init_obj.register_initializer(initialize_godotrobot_module);
+	init_obj.register_terminator(uninitialize_godotrobot_module);
+	init_obj.set_minimum_library_initialization_level(MODULE_INITIALIZATION_LEVEL_SCENE);
+
+	return init_obj.init();
+}
+}
+
+
