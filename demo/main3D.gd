76c92a058d9bb8fdcb5266a8e5ece444798126fb
Andrew Mattson <acmattson3@alaska.edu>
2024-03-23 18:52:20 -0800

    Removed unused camera logic.

--- demo/main3D.gd
+++ demo/main3D.gd
@@ -4,15 +4,8 @@ extends Node3D
 const camera_external_id = 0
 @onready var camera_onboard = $DemoRobot3D/RobotOnboardView
 const camera_onboard_id = 1
-@onready var camera_pivot = $DemoRobot3D/CameraPivot
-@onready var robot = $DemoRobot3D
 
 var curr_camera: int = 0
-var look_at_point: Vector3
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	look_at_point = robot.global_position
 
 func _process(_delta):
 	# Camera switching
