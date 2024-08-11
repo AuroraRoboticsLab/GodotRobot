24a39512cd84f18ab149ef7ed9be174f7e0cc045
Andrew Mattson <acmattson3@alaska.edu>
2024-08-11 14:19:27 -0800

    Fixed editor bug with @tool where dirtballs had a missing field.

--- demo/terrain/dirtball.gd
+++ demo/terrain/dirtball.gd
@@ -3,7 +3,7 @@ extends RigidBody3D
 @export var bucket_count = 0  # number of buckets that we are currently inside (0 == free on terrain)
 @export var hopper_count = 0 # number of hoppers that we are currently inside
 
-var terrain = null # Set by spawner upon creation
+@export var terrain: TerrainSim = null # Set by spawner upon creation
 var despawned: bool = false
 
 func _ready():
